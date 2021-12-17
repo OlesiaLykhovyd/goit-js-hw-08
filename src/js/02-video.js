e072c6410ccbe6f2527e98db9f4aa1f202fb76e0
OlesiaLykhovyd <olesyalykhovyd@gmail.com>
2021-12-17 19:29:57 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+const onTimeUpdate = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', onTimeUpdate);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
